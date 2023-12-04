09fc3c226fa0f341999737410602fa5e418bb392
DerianSeto <144382635+DerianSeto@users.noreply.github.com>
2023-12-04 11:04:37 -0700

    Beautified HTML part 2/2
    Use this one. It is more editable.

--- Dropdown.js
+++ Dropdown.js
@@ -11,6 +11,33 @@
 </a>
 */
 
+//Bro, call me icarus because I'm proud of this
+function initialize(){
+	//Adds events to drop .Mains
+	let dropBtns = document.getElementsByClassName("MenuDropBtns");
+	for(let i = 0; i < dropBtns.length; i++){
+		dropBtns[i].addEventListener('mouseenter', function(){Drop(dropBtns[i].getElementsByClassName("Mains")[0].id, false)});
+		dropBtns[i].addEventListener('mouseleave', function(){Drop(dropBtns[i].getElementsByClassName("Mains")[0].id, true)});
+		//The style stuff
+		dropBtns[i].addEventListener('mouseenter', function(){lookinGood(dropBtns[i+1].id, false)});
+		dropBtns[i].addEventListener('mouseleave', function(){lookinGood(dropBtns[i+1].id, true)});
+	}
+	
+	//Adds evetns to drop .Insets
+	let Mains = document.getElementsByClassName("Mains");
+	for(let i = 0; i < Mains.length; i++){
+		let MainsLi = Mains[i].getElementsByTagName("li");
+		for(let j = 0; j < MainsLi.length; j++){
+			if(getParent(MainsLi[j].id).id == "Insets"){
+				MainsLi.splice(j, 1);
+			}else{
+				MainsLi[j].addEventListener('mouseenter', function(){Drop(MainsLi[j].getElementsByClassName("Insets")[0].id, false)});
+				MainsLi[j].addEventListener('mouseleave', function(){Drop(MainsLi[j].getElementsByClassName("Insets")[0].id, true)});
+			}
+		}
+	}
+}
+
 function Drop(id, Dropped){ //Opens and closes dropdown menus | Call from HTML using ^
 	let toDrop = document.getElementById(id);						//Initializes the element toDrop
 	let Parent = getParent(id);										//Initializes Parent to getParent(id)
@@ -57,15 +84,25 @@ function getParent(id){ //Retrieves the parent element of (id)
 	return "Failed";											//Returns a definitive failure value | Check against (Gramps) for double embedded lists
 }
 
+function setLabelTime(){
+	let date = new Date;																	//Gets the date from the computer
+	document.getElementById("DateLabel").innerHTML = (date.getFullYear().toString() + "/"	//Gives DateLabel the year (XXXX/)
+		+ (date.getMonth()+1).toString() + "/"												//Gives DateLabel the Month (XX/) | Between 0 and 11
+		+ date.getDate().toString());														//Gives DateLabel the Day (XX)
+}
 
+function Search(){
+	alert("This doesn't work lol");
+	window.open("http://www.google.ca")
+}
 
-
-function getParent(id){ //Retrieves the parent element of (id)
-	let Lists = document.getElementsByTagName("ul");			//Creates a list of unordered lists, to check against the (id)
-	for(let i=0; i<Lists.length; i++){ //Loops through the uls
-		if(Lists[i].contains(document.getElementById(id))){ //Checks if the item is (id)'s parent
-			return Lists[i];									//Returns the parent
-		}
+function lookinGood(id, LBordered){ //Adds border to left side of div (id) | Use the id of the button one left of the one being used
+	let leftBtn = document.getElementById(id);
+	if(LBordered){
+		leftBtn.style.borderLeft="none";
+		leftBtn.style.width = "20%";
+	}else{
+		leftBtn.style.borderLeft="5px solid #070E0E";
+		leftBtn.style.width = "calc(20% - 5px)";
 	}
-	return "Failed";											//Returns a definitive failure value | Check against (Gramps) for double embedded lists
 }
